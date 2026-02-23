fe41aec158d911c07c95ef051d8207a083e4f8df
Andreszuluga <carlosandrescespedesz22@gmail.com>
2026-02-23 18:12:42 -0500

    Add project files.

---
+++ POO1time.backen/Time.cs
@@ -0,0 +1,11 @@
+﻿namespace POO1time.backen
+{
+    public class Time
+    {
+        private int hour;
+        private int minute;
+        private int second;
+        private int microsecond;
+        
+    }
+}
